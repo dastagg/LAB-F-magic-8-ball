38105952bb8acb509e0065c9c23127a94ea4acfd
dastagg:opus5 <dastagg@stagglinux.com>
2022-02-14 22:31:13 -0500

    Section 8: Step 1 and 2 - Completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      const MaterialApp(
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Center(
+            child: Text(
+              'Ask Me Anything',
+            ),
+          ),
+      ),
+      body: Container(),
+    );
+  }
+}
