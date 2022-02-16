7f4d3b65c1f8e113822f0ee5809fe046959aa988
dastagg:opus5 <dastagg@stagglinux.com>
2022-02-15 20:37:39 -0500

    Add AnimatedContainer.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(
+void main() =>
+    runApp(
       const MaterialApp(
         home: BallPage(),
       ),
@@ -15,13 +16,13 @@ class BallPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
-        appBar: AppBar(
-          backgroundColor: Colors.blue[800],
-          title: const Center(
-            child: Text(
-              'Ask Me Anything',
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.blue[800],
+        title: const Center(
+          child: Text(
+            'Ask Me Anything',
           ),
+        ),
       ),
       body: const Ball(),
     );
@@ -33,27 +34,32 @@ class Ball extends StatefulWidget {
 
   @override
   _BallState createState() => _BallState();
-
 }
 
 class _BallState extends State<Ball> {
-
+  bool selected = false;
   int ballNumber = 1;
-
-  void changeBallImage() {
-    setState(() {
-      ballNumber = Random().nextInt(5) + 1;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        onPressed: () {
-          changeBallImage();
-        },
-        child: Image.asset('images/ball$ballNumber.png'),
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+          ballNumber = Random().nextInt(5) + 1;
+        });
+      },
+      child: Center(
+        child: AnimatedContainer(
+          width: selected ? 400.0 : 200.0,
+          height: selected ? 400.0 : 200.0,
+          color: selected ? Colors.blue : Colors.blue,
+          alignment:
+          selected ? Alignment.center : AlignmentDirectional.topCenter,
+          duration: const Duration(seconds: 2),
+          curve: Curves.fastOutSlowIn,
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
       ),
     );
   }
