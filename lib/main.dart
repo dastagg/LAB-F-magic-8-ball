98521bb42b23a2211e6fc97fdb28cc2a09fe4515
dastagg:opus5 <dastagg@stagglinux.com>
2022-02-15 20:17:37 -0500

    Project Completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -14,14 +16,45 @@ class BallPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.blue,
         appBar: AppBar(
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.blue[800],
           title: const Center(
             child: Text(
               'Ask Me Anything',
             ),
           ),
       ),
-      body: Container(),
+      body: const Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+
+}
+
+class _BallState extends State<Ball> {
+
+  int ballNumber = 1;
+
+  void changeBallImage() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          changeBallImage();
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
     );
   }
 }
